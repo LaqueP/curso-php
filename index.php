3ebbd1dcf595dc743ebaef41c113aba8baddbb6b
LaqueP <creyero12@gmail.com>
2024-07-14 19:43:07 +0200

    añadimos index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+echo "Hola de nuevo<br>";
+
+$valor1= 12;
+$valor2= 50;
+$resultado= $valor1 + $valor2;
+
+echo "El total es $resultado<br>";
+
+
+$nombre = "Carlos";
+$apellido = "Reyero";
+
+echo "Mi nombre es $nombre $apellido";
