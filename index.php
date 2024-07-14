5c6b781c4877356e05e947beea2e04328c865c8f
LaqueP <creyero12@gmail.com>
2024-07-14 23:34:53 +0200

    añadimos testeo var_dump y pruebas de variables

--- index.php
+++ index.php
@@ -1,14 +1,26 @@
 <?php 
-echo "Hola de nuevo<br>";
+echo "Hola de nuevo \n";
 
 $valor1= 12;
 $valor2= 50;
 $resultado= $valor1 + $valor2;
 
-echo "El total es $resultado<br>";
+echo "El total es $resultado \n";
 
 
 $nombre = "Carlos";
 $apellido = "Reyero";
 
-echo "Mi nombre es $nombre $apellido";
+echo "Mi nombre es $nombre $apellido \n";
+
+$alumnos =[
+    "Carlos" => 31,
+    "Desi" => 29,
+    "Angelita" => 68
+];
+
+var_dump($alumnos); // te muestra toda la información de una variable
+
+define("NUMERO_PI", 3.14);
+
+echo NUMERO_PI;
